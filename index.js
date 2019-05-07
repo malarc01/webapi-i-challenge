3538ff03caefcfb4ba0473b78ff09c59d7097b98
Your Name <melky@Melkys-iMac.local>
2019-05-07 11:47:55 -0400

    /users is working used JSON + send()

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // implement your API here
 //import express from 'express'; // ES2015 module import
 const express = require('express'); //Common JS module import
-
+const database = require('./data/db')
 const server = express();
+
+server.get('/', (request,response)=>{
+  response.send('it is alive');
+
+});
+
+// GET /hubs => return a list of hubs in JSON format
+server.get('/users', (req,res)=>{
+  database.find().then(users=>{
+    res.json(users);
+  }).catch(err=>{res.json({error:err, message:'Something broke'})})
+})
 
 server.listen(5000, ()=>{
   console.log('API RUNNING PORT 5K')
