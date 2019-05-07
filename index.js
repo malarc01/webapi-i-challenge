0aa620f28881aaba5e62c7b93532d64543cb3054
Your Name <melky@Melkys-iMac.local>
2019-05-07 11:17:23 -0400

    yarn add express & server is up and running &console logging

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // implement your API here
+//import express from 'express'; // ES2015 module import
+const express = require('express'); //Common JS module import
+
+const server = express();
+
+server.listen(5000, ()=>{
+  console.log('API RUNNING PORT 5K')
+});
